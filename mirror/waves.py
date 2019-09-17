0a74d229a2f0a54d3afcf03fcb5dd30c13e73751
pseudopi-uno <mitchell5799@gmail.com>
2019-09-17 03:02:44 -0700

    updated waves.py

--- mirror/waves.py
+++ mirror/waves.py
@@ -5,6 +5,7 @@
 
 import time
 import board
+import copy
 import adafruit_dotstar as dotstar
 import random
 from colour import Color
@@ -27,26 +28,34 @@ pix1 = []
 pix2 = []
 count = 0
 cycle = 20
+baseHue = Color("blue").hue
+
 
 for i in range(numpixels):
-    pix1.append(limit(random.gauss(0.4, 0.5)))
-    pix2.append(limit(random.gauss(0.4, 0.5)))
+    pix1.append(Color(hsl=(limit(random.gauss(baseHue, 0.05)),limit(random.gauss(0.7, 0.3)),limit(random.gauss(0.4, 0.5)))))
+    pix2.append(Color(hsl=(limit(random.gauss(baseHue, 0.05)),limit(random.gauss(0.7, 0.3)),limit(random.gauss(0.4, 0.5)))))
 
 while True:                  # Loop forever
     for i in range(numpixels):
-        n = (count+(i%13))%(cycle)
+        n = (count+(i%17))%(cycle)
         if(n == 0):
-            strip[i] = parseColor(Color(hsl=(0,1.0,pix2[i]*0.7)))
-            pix1[i] = pix2[i]
-            if random.uniform(0,2) > 1.0:
-                pix2[i] = limit(random.gauss(0.5, 0.7))
+
+            pix1[i] = copy.copy(pix2[i])
+            strip[i] = parseColor(pix1[i])
+            if random.uniform(0,2) > 1.3:
+                hue = limit(random.gauss(baseHue, 0.05))
+                sat = limit(random.gauss(0.8, 0.2))
+                lum = limit(random.gauss(0.3, 0.3))
+                pix2[i] = Color(hsl=(hue,sat,lum))
             else:
-                pix2[i] = 0
+                pix2[i] = Color(hsl=(pix2[i].hue, pix2[i].saturation, 0))
         else:
-            brightness = lerp(pix1[i], pix2[i], float(n)/(cycle))
-            strip[i] = parseColor(Color(hsl=(0, 1.0, brightness*0.7)))
+            hue = lerp(pix1[i].hue, pix2[i].hue, float(n)/(cycle))
+            sat = lerp(pix1[i].saturation, pix2[i].saturation, float(n)/(cycle))
+            lum = lerp(pix1[i].luminance, pix2[i].luminance, float(n)/(cycle))
+            strip[i] = parseColor(Color(hsl=(hue, sat, lum)))
 
     count += 1
     strip.show()
         # Update strip
-    time.sleep(1.0 / 500)
+    #time.sleep(1.0 / 500)
